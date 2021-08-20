404dd5e08b20f290e3802b1259227a3679adf166
Stefan Miklosovic <smiklosovic@apache.org>
2021-08-20 16:37:20 +0200

    next

--- src/java/org/apache/cassandra/cql3/statements/AlterTableStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/AlterTableStatement.java
@@ -149,7 +149,7 @@ public class AlterTableStatement extends SchemaAlteringStatement
                 }
             case ADD:
                 assert columnName != null;
-                if (meta.isCompactTable())
+                if (meta.isDense() || meta.isCompactTable())
                     throw new InvalidRequestException("Cannot add new column to a COMPACT STORAGE table");
 
                 cfm = meta.copy();
